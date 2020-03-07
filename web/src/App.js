03035ed35163b4a2cf197ca86e5ea868af42a688
Bogdan Guna <bogdan.guna@gmail.com>
2020-03-07 15:37:14 +0000

    chore(web): setup of react router and navigation

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Ben Goro Photography</h1>
+                    <ul className='navigation'>
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/about'>About</NavLink></li>
+                        <li><NavLink to='/contact'>Contact</NavLink></li>
+                    </ul>
+
+                    <div className='content'>
+                        <Route path='/' component={Home}/>
+                        <Route path='/about' component={About}/>
+                        <Route path='/contact' component={Contact}/>
+                    </div>
+                </div>
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
---
+++ web/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render () {
+        return (
+            <div>
+                <h2>got questions?</h2>
+
+                <p>Reach out to <a href="mailto:benjiro.goro@gmail.com">benjiro.goro@gmail.com</a>.</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
