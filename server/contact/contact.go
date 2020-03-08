fe5d490b4afae98fd4d03f03a0567d2210f1fec9
Bogdan Guna <bogdan.guna@gmail.com>
2020-03-08 01:08:12 +0000

    feat(server): impl storage for contact msg

--- server/contact/contact.go
+++ server/contact/contact.go
@@ -1,11 +1,13 @@
 package contact
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // contactMessage represents the structure of an incoming Contact message.
@@ -18,6 +20,7 @@ type contactMessage struct {
 // HandleNewMsg handles incoming messages from the Contact tab.
 func HandleNewMsg() func(context *gin.Context) {
 	return func(context *gin.Context) {
+		db, _ := sql.Open("sqlite3", "./storage/benxgoro.db")
 		message := contactMessage{}
 		rawContextData, err := context.GetRawData()
 		if err != nil {
@@ -27,10 +30,23 @@ func HandleNewMsg() func(context *gin.Context) {
 
 		err = json.Unmarshal(rawContextData, &message)
 		if err != nil {
-			log.Println("Error processing request")
+			log.Println("Error processing request.", err)
 			context.JSON(http.StatusBadRequest, gin.H{"status": "fail"})
 		}
 		log.Printf("Received contact message: %+v", message)
+		statement, err := db.Prepare("INSERT INTO contact (name, email, message) VALUES (?, ?, ?)")
+		if err != nil {
+			log.Println("Error preparing to store contact message.", err)
+			context.JSON(http.StatusBadRequest, gin.H{"status": "fail"})
+		}
+
+		_, err = statement.Exec(message.Name, message.Email, message.Message)
+		if err != nil {
+			log.Println("Error storing contact message.", err)
+			context.JSON(http.StatusBadRequest, gin.H{"status": "fail"})
+		}
+
+		log.Println("Contact message stored.")
 		context.JSON(http.StatusOK, gin.H{"status": "success"})
 	}
 }
